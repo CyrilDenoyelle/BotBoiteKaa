f982ba3859c03b15ac28b73593c6124ac396d8d5
CyrilD <cyril.denoyelle@morning.com>
2018-10-14 23:33:29 +0200

    trying to create new client each time

--- db/postgresClient.js
+++ db/postgresClient.js
@@ -1,29 +1,26 @@
 const { Client } = require('pg');
-const client = new Client({
+const client = () => new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: true,
 });
 
 const reunionAddPg = (params) => {
-  client.connect();
-  client.query(`INSERT INTO reunion VALUES('${params.id}', '${params.name}', '${params.date}', '${params.user_id}', '${params.created_at}');`, (err, res) => {
+  client().connect().query(`INSERT INTO reunion VALUES('${params.id}', '${params.name}', '${params.date}', '${params.user_id}', '${params.created_at}');`, (err, res) => {
     if (err) console.log(err);
-    client.end();
+    pg.end();
     return res;
-  });
+  }).end()
 }
 
 const reunionList = () => {
   return new Promise((resolve, rej) => {
-    client.connect();
-    client.query(`SELECT * FROM reunion;`, (err, res) => {
+    client().connect().query(`SELECT * FROM reunion;`, (err, res) => {
       if (err) {
         console.log('error', err);
         return null;
       }
       resolve(res.rows);
-      client.end();
-    });
+    }).end();
   });
 };
 
