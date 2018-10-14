e685e4137aa8e7186243dcade2c1d2e7851f4fa6
CyrilD <cyril.denoyelle@morning.com>
2018-10-14 18:41:16 +0200

    remove uuid

--- db/postgresClient.js
+++ db/postgresClient.js
@@ -1,5 +1,4 @@
 const { Client } = require('pg');
-const uuid = require('uuid');
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: true,
@@ -7,9 +6,10 @@ const client = new Client({
 
 const reunionAddPg = (params) => {
   client.connect();
-  client.query(`INSERT INTO reunion VALUES('${uuid()}', '${params.name}', '${params.date}', '${params.user_id}', '${params.created_at}');`, (err, res) => {
+  client.query(`INSERT INTO reunion VALUES('${params.id}', '${params.name}', '${params.date}', '${params.user_id}', '${params.created_at}');`, (err, res) => {
     if (err) console.log(err);
     client.end();
+    return res;
   });
 }
 
