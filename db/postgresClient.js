f308e66eaf2c85e0c5b2bf303c1d14ce92f41ceb
CyrilD <cyril.denoyelle@morning.com>
2018-10-14 21:33:03 +0200

    test other way sql select

--- db/postgresClient.js
+++ db/postgresClient.js
@@ -14,22 +14,45 @@ const reunionAddPg = (params) => {
 }
 
 const reunionList = () => {
-  client.connect();
-  client.query(`SELECT * FROM reunion;`, (err, res) => {
+  // client.connect();
+  // client.query(`SELECT * FROM reunion;`, (err, res) => {
+  //   if (err) {
+  //     console.log(err);
+  //     return null;
+  //   }
+  //   r = []
+  //   for (let row of res.rows) {
+  //     console.log(row);
+  //     r.push(row);
+  //   }
+  //   client.end();
+  //   return r;
+  // }).then((e) => {
+  //   console.log(e);
+  // })
+
+  const results = [];
+  // Get a Postgres client from the connection pool
+  client.connect((err, client, done) => {
+    // Handle connection errors
     if (err) {
+      done();
       console.log(err);
-      return null;
+      return res.status(500).json({ success: false, data: err });
     }
-    r = []
-    for (let row of res.rows) {
-      console.log(row);
-      r.push(row);
-    }
-    client.end();
-    return r;
-  }).then((e) => {
-    console.log(e);
-  })
+    // SQL Query > Select Data
+    const query = client.query('SELECT * FROM reunion ORDER BY id ASC;');
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results
+    query.on('end', () => {
+      done();
+      return res.json(results);
+    });
+  });
+
 }
 
 module.exports = {
