764226e5bef550c33e6670616ce896ebaebaef94
CyrilD <cyril.denoyelle@morning.com>
2018-10-14 18:47:53 +0200

    add reunionList function in botResponsTemplate

--- helpers/botResponseTemplates.js
+++ helpers/botResponseTemplates.js
@@ -1,6 +1,10 @@
 
 const reunionList = (msg, allReunions) => {
-  msg.reply(`coucou`);
+  stres = [];
+  allReunions.map(r => {
+    stres.push(`${r.id} | ${r.name} | ${r.date}`);
+  });
+  msg.reply(` id | name | date \n ${stres.join('\n')}`);
 }
 
 module.exports = {
