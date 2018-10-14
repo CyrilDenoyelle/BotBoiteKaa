45034074984e37bb25d0da2876af436d1e4f1693
CyrilD <cyril.denoyelle@morning.com>
2018-10-14 16:07:44 +0200

    add uuid

--- db/postgresClient.js
+++ db/postgresClient.js
@@ -1,5 +1,5 @@
 const { Client } = require('pg');
-
+const uuid = require('uuid');
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: true,
@@ -12,7 +12,7 @@ const reunionAddPg = (params) => {
 
 
 
-  client.query(`INSERT INTO reunion VALUES('${params.name}', '${params.date}', '${params.user_id}', '${params.created_at}');`, (err, res) => {
+  client.query(`INSERT INTO reunion VALUES('${uuid()}', '${params.name}', '${params.date}', '${params.user_id}', '${params.created_at}');`, (err, res) => {
     if (err) console.log(err);
     // for (let row of res.rows) {
     //   console.log(JSON.stringify(row));
