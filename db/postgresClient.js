ac7ce314962dd3f77d7397cd37f25c425e3e0881
CyrilD <cyril.denoyelle@morning.com>
2018-10-14 16:11:05 +0200

    clead reunionAddPg

--- db/postgresClient.js
+++ db/postgresClient.js
@@ -6,21 +6,11 @@ const client = new Client({
 });
 
 const reunionAddPg = (params) => {
-
   client.connect();
-  // ${process.env.DB_DATABASE}
-
-
-
   client.query(`INSERT INTO reunion VALUES('${uuid()}', '${params.name}', '${params.date}', '${params.user_id}', '${params.created_at}');`, (err, res) => {
     if (err) console.log(err);
-    // for (let row of res.rows) {
-    //   console.log(JSON.stringify(row));
-    // }
     client.end();
   });
-
-  // client.end();
 }
 
 module.exports = {
