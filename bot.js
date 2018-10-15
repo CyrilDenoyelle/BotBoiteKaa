14c1bbee795294750a46c245b88c19f3f3427dfd
CyrilD <cyril.denoyelle@morning.com>
2018-10-15 22:05:41 +0200

    fix reunion handler spread + refactoring prod detection

--- bot.js
+++ bot.js
@@ -8,13 +8,10 @@ express().get('/', (req, res) => { res })
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prod = process.env.DATABASE_URL ? true : false;
 
 const { msgHandler } = require('./helpers/messages.js');
-const {
-  cancel, // testing
-  newReunionSeeds,
-  list,
-} = require('./helpers/reunionsHandler.js');
+const reunion = require('./helpers/reunionsHandler.js');
 
 const {
   listReunion,
@@ -29,27 +26,27 @@ client.on('message', msg => {
   msgHandler(msg);
 });
 
-
-[
-  '!reunion R&D 2018-10-14T17:23:00',
-  '!reunion coucou 2018-10-14T17:24:00',
-  '!reunion trulu 2018-10-14T17:25:00'
-].map(e => {
-  newReunionSeeds(e);
-})
+if (!prod) {
+  [
+    '!reunion R&D 2018-10-14T17:23:00',
+    '!reunion coucou 2018-10-14T17:24:00',
+    '!reunion trulu 2018-10-14T17:25:00'
+  ].map(e => {
+    reunion.handlers.newReunionSeeds(e);
+  });
+}
 
 
 setInterval(() => {
   const now = new Date();
   // console.log('getAllReunions', getAllReunions());
-  list().then(e => {
+  reunion.handlers.list().then(e => {
     e.map(row => {
       if (row && row.date > now && !row.isDeleted) {
         // e.isDeleted = true;
-
-        if (process.env.DATABASE_URL) {
+        if (prod) {
           console.log(`@everyone c'est l'heure de ${row.name}`);
-          // client.channels.get('500978775878664195').send(`${process.env.DATABASE_URL ? '@veryone' : '@veryone'} c'est l'heure de ${e.name}`);
+          // client.channels.get('500978775878664195').send(`${process.env.DATABASE_URL ? '@veryone' : '@veryone'} c'est l'heure de ${row.name}`);
         }
       }
     })
