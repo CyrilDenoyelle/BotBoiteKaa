ff28b1f98d9d0c4bbd7899e49cb5ce4f4aaa78c6
CyrilD <cyril.denoyelle@morning.com>
2018-10-16 23:10:59 +0200

    la grosse up

--- helpers/reunionsHandler.js
+++ helpers/reunionsHandler.js
@@ -3,76 +3,67 @@ const pgc = require('../db/postgresClient');
 const uuid = require('uuid');
 const msgTutoReunion = `pour utiliser la fonction de reunion votre message doit ressembler a ça biatch: \n "!reunion pour sans guillemet 1995-12-17T13:25:00" \n (attention ce truk va faire un "@"everyone sur le discord.) \n pour annuler une reunion: c\'est tres simple aussi !reunionList affiche toute les reunion il suffi de faire un !reunionCancel ID_REUNION.`
 const prod = process.env.DATABASE_URL ? true : false;
-const handlers = {
 
+
+paramsFormaters = {
   create: (msg) => {
-    const args = msg.content.split(' ').slice(2).join(' ').split(', ');
-    console.log('args ', args);
-    if (args.length >= 2) {
-      msg.reply(`reunion set au ${args[0]} pour ${args[1]}`);
-
-      const now = new Date().valueOf() + 36000;
-      const argsDate = new Date(args[1]);
-      reuDate = new Date(Date.UTC(argsDate.getFullYear(), argsDate.getMonth(), argsDate.getDate(), argsDate.getHours(), argsDate.getMinutes(), argsDate.getSeconds()));
-
-      const params = {
-        id: uuid(),
-        name: args[0],
-        date: reuDate,
-        user_id: msg.author.id,
-        created_at: now
-      }
-
-      if (prod) {
-        console.log('pgc.createReunion reunionHandler.js', pgc.createReunion(params));
-      } else {
-        reunions.push(params);
-        console.log('push reunionHandler.js', reunions);
-      }
-    } else {
-      msg.reply(msgTutoReunion);
-    }
-  },
-
-  list: () => {
-    if (prod) {
-      return pgc.listReunion();
-    } else {
-      return new Promise(() => {
-        res(reunions);
-      });
-    }
-  },
-
-  cancel: (msg) => {
-    const id = msg.split(' ')[2];
-    if (prod) {
-      pgc.getReunionById(id).then(reunion => {
-        console.log(`reunion ${id}`, reunion);
-        return reunion;
-      })
-    } else {
-      return reunions;
-    }
-  },
-
-  newReunionSeeds: (msg) => {
     const args = msg.split('!reunion ')[1].split(' ');
-    console.log('args ', args);
     if (args.length >= 2) {
       const now = new Date().valueOf() + 36000;
       const argsDate = new Date(args[1]);
-      reuDate = new Date(Date.UTC(argsDate.getFullYear(), argsDate.getMonth(), argsDate.getDate(), argsDate.getHours(), argsDate.getMinutes(), argsDate.getSeconds()));
+      const reuDate = new Date(Date.UTC(argsDate.getFullYear(), argsDate.getMonth(), argsDate.getDate(), argsDate.getHours(), argsDate.getMinutes(), argsDate.getSeconds()));
 
-      const params = {
+      return params = {
         id: uuid(),
         name: args[0],
         date: reuDate,
         user_id: 11111111111111,
         created_at: now
       }
-      reunions.push(params);
+    } else {
+      return false;
     }
+  },
+  cancel: (msg) => {
+    const id = msg.split(' ')[2]
+    if (id) {
+      return id;
+    }
+  }
+}
+
+const handlers = {
+  create: (msg) => {
+    params = paramsFormaters.create();
+    return params ? pgc.createReunion(params) : tuto(msg);
+  },
+
+  list: () => {
+    return pgc.listReunion();
+  },
+
+  cancel: (msg) => {
+    const id = paramsFormaters.cancel(msg)
+    pgc.getReunionById(id).then(reunion => {
+      console.log(`reunion ${id}`, reunion);
+      return reunion;
+    })
+  }
+}
+
+localHandlers = {
+  create: (msg) => {
+    const params = paramsFormaters.create(msg);
+    params ? reunions.push(params) : tuto(msg);
+  },
+  list: () => {
+    return new Promise(() => {
+      res(reunions);
+    });
+  },
+  cancel: (msg) => {
+    const id = msg.split(' ')[2];
+    return reunions.filter({ id });
   }
 }
 
@@ -82,16 +73,25 @@ tuto = (msg) => {
 
 msgHandler = (msg) => {
   s = msg.content.split(' ')[1];
-
-  if (s && Object.keys(handlers).includes(s)) {
-    handlers[s](msg);
+  // [`${prod ? 'h' : 'localH'}andlers`]
+  if (prod) {
+    if (s && Object.keys(handlers).includes(s)) {
+      handlers[s](msg);
+    } else {
+      tuto(msg);
+    }
   } else {
-    tuto(msg);
+    if (s && Object.keys(localHandlers).includes(s)) {
+      localHandlers[s](msg);
+    } else {
+      tuto(msg);
+    }
   }
 }
 
 module.exports = {
   msgHandler,
   handlers,
+  localHandlers,
   tuto
 }
