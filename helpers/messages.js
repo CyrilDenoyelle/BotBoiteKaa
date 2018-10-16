e8477764f7bf863280a95875f0fbcb54552f412b
CyrilD <cyril.denoyelle@morning.com>
2018-10-16 23:06:13 +0200

    toLowerCase msg includes for better reading of messages

--- helpers/messages.js
+++ helpers/messages.js
@@ -5,13 +5,13 @@ const botMsg = require('./botResponseTemplates');
 msgHandler = (msg) => {
   if (msg.author.id !== '499347187856179250') {
     console.log('msg.author.id', msg.author.id);
-    if (msg.content.includes('pong') || msg.content.includes('Pong')) {
+    if (msg.content.toLowerCase().includes('pong')) {
       msg.channel.send('Ping', {
         tts: true
       });
     }
-    if (msg.content.includes('ping') || msg.content.includes('Ping')) {
-      const biatch = Math.random() > 0.89 ? 'biatch' : '';
+    if (msg.content.toLowerCase().includes('ping')) {
+      const biatch = Math.random() >= 0.75 ? 'biatch' : '';
       msg.channel.send(`Pong! ${biatch}`, {
         tts: true
       });
