fdb7b6bad1af0693394af4cae2efe3a6982d42fc
CyrilD <cyril.denoyelle@morning.com>
2018-10-16 16:57:01 +0200

    add no response to self

--- helpers/messages.js
+++ helpers/messages.js
@@ -3,20 +3,21 @@ const reunion = require('./reunionsHandler.js');
 const botMsg = require('./botResponseTemplates');
 
 msgHandler = (msg) => {
-
-  console.log('msg', msg);
-  console.log('self', this);
-  // if (msg.content.includes('pong') || msg.content.includes('Pong')) {
-  //   msg.channel.send('Ping', {
-  //     tts: true
-  //   });
-  // }
-  if (msg.content.includes('ping') || msg.content.includes('Ping')) {
-    const biatch = Math.random() > 0.89 ? 'biatch' : '';
-    msg.channel.send(`Pong! ${biatch}`, {
-      tts: true
-    });
+  if (msg.author.id !== '499347187856179250') {
+    console.log('msg.author.id', msg.author.id);
+    if (msg.content.includes('pong') || msg.content.includes('Pong')) {
+      msg.channel.send('Ping', {
+        tts: true
+      });
+    }
+    if (msg.content.includes('ping') || msg.content.includes('Ping')) {
+      const biatch = Math.random() > 0.89 ? 'biatch' : '';
+      msg.channel.send(`Pong! ${biatch}`, {
+        tts: true
+      });
+    }
   }
+
   if (msg.content.includes('bite') || msg.content.includes('queue')) {
     msg.react("🍆");
   }
