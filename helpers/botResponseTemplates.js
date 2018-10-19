9ddd36a15a1ad850dddd776ab7c14449cf5c1bfc
CyrilD <cyril.denoyelle@morning.com>
2018-10-19 20:49:23 +0200

    add tutos + change const name for clarity

--- helpers/botResponseTemplates.js
+++ helpers/botResponseTemplates.js
@@ -1,12 +1,26 @@
 
 const listReunion = (msg, allReunions) => {
-  stres = [];
+  list = [];
   allReunions.map(r => {
-    stres.push(`${r.id} | ${r.name} | ${r.date}`);
+    list.push(`${r.id} | ${r.name} | ${r.date}`);
   });
-  msg.reply(`\n id | name | date \n ${stres.join('\n')}`);
+  msg.reply(`\n id | name | date \n ${list.join('\n')}`);
+}
+
+const createReunion = (msg, created) => {
+  msg.reply(`c'est vous l'doc doc: \n id | name | date \n ${created.id} | ${created.name} | ${created.date}`);
+}
+
+const tutos = {
+  createReunion: `pour creer une reunion votre message doit ressembler a ça biatch: \n "!reunion pourquoi sans apostrophe 1995-12-17T13:25:00" \n (attention ce truk va faire un "@"everyone sur le discord a l'heure donnée.)`,
+  deleteReunion: `pour annuler une reunion: "!reunionCancel ID_REUNION." (!reunionList affiche toutes les reunions)`,
+  listReunion: `!reunionList affiche toutes les reunions`,
+  reunion: `pour utiliser la fonction de reunion votre message doit ressembler a ça biatch: \n "!reunion pourquoi sans apostrophe 1995-12-17T13:25:00" \n (attention ce truk va faire un "@"everyone sur le discord.) \n pour annuler une reunion: c\'est tres simple aussi !reunionList affiche toute les reunion il suffi de faire un !reunionCancel ID_REUNION.`,
+  helpReunion: `azerjkqsldfkxcvnorutapiurqkdjwbcxbkhfdgaurieysqgojfsnbmsfh`
 }
 
 module.exports = {
-  listReunion
+  listReunion,
+  createReunion,
+  tutos
 }
