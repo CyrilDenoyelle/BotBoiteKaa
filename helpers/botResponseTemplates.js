b401c70cd39419074d6cb996597cc3fdd5c7bc7a
CyrilD <cyril.denoyelle@morning.com>
2018-10-19 23:07:56 +0200

    fix senteces for good ?

--- helpers/botResponseTemplates.js
+++ helpers/botResponseTemplates.js
@@ -12,10 +12,10 @@ const createReunion = (msg, created) => {
 }
 
 const tutos = {
-  createReunion: `pour creer une reunion votre message doit ressembler a ça biatch: \n "!reunion pourquoi sans apostrophe 1995-12-17T13:25:00" \n (attention ce truk va faire un "@"everyone sur le discord a l'heure donnée.)`,
-  deleteReunion: `pour annuler une reunion: "!reunionCancel ID_REUNION." (!reunionList affiche toutes les reunions)`,
-  listReunion: `!reunionList affiche toutes les reunions`,
-  reunion: `pour utiliser la fonction de reunion votre message doit ressembler a ça biatch: \n "!reunion pourquoi sans apostrophe 1995-12-17T13:25:00" \n (attention ce truk va faire un "@"everyone sur le discord.) \n pour annuler une reunion: c\'est tres simple aussi "!reunion list" affiche toute les reunions il suffi de faire un "!reunion cancel" ID_REUNION.`,
+  createReunion: `pour creer une reunion votre message doit ressembler a ça biatch: \n "!reunion create pourquoi sans apostrophe 1995-12-17T13:25:00" \n (attention ce truk va faire un "@"everyone sur le discord a l'heure donnée.)`,
+  deleteReunion: `pour annuler une reunion: "!reunion cancel ID_REUNION." (!reunion list affiche toutes les reunions)`,
+  listReunion: `!reunion list affiche toutes les reunions`,
+  reunion: `pour utiliser la fonction de reunion votre message doit ressembler a ça biatch: \n "!reunion create pourquoi sans apostrophe 1995-12-17T13:25:00" \n (attention ce truk va faire un "@"everyone sur le discord.) \n pour annuler une reunion: c\'est tres simple aussi "!reunion list" affiche toute les reunions il suffit de faire un "!reunion cancel ID_REUNION".`,
   helpReunion: `azerjkqsldfkxcvnorutapiurqkdjwbcxbkhfdgaurieysqgojfsnbmsfh`
 }
 
