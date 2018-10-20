ec0cbd0692c893b86cf85fdf2d406e4cac4e3f36
CyrilD <cyril.denoyelle@morning.com>
2018-10-20 15:37:22 +0200

    trying to pasre whiteList

--- helpers/middlewares/isWhiteListGuild.js
+++ helpers/middlewares/isWhiteListGuild.js
@@ -2,7 +2,7 @@ const white_list = process.env.WHITE_LIST;
 
 module.exports = (id) => {
   console.log('white_list, id', white_list, id);
-  console.log('typeof white_list', typeof white_list);
-  // return white_list.includes(id)
+  console.log('typeof JSON.parse(white_list)', typeof JSON.parse(white_list));
+  // return JSON.parse(white_list).includes(id)
   return true;
 }
