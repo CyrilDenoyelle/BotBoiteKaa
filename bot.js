53e4a455763064dd81097a805af6a3d42bec15de
CyrilD <cyril.denoyelle@morning.com>
2018-10-20 13:18:11 +0200

    testing for date now

--- bot.js
+++ bot.js
@@ -24,12 +24,13 @@ client.on('ready', () => {
 
   client.channels.get(process.env.UP_GEN).send('@here COOLCOOLCOOL');
   const intervalFunc = () => {
-    const now = new Date().addHours(2);
-    console.log('now', now);
+    const now = new Date()
+    const realNow = now.addHours(2);
+    console.log('realNow', realNow);
     reunion[`${prod ? 'h' : 'localH'}andlers`].list(true).then(e => {
       e.payload.map(row => {
         console.log('new Date(row.date)', new Date(row.date));
-        if (row && new Date(row.date) < now && !row.is_deleted) {
+        if (row && new Date(row.date) < realNow && !row.is_deleted) {
           console.log('yes row.is_deleted', row.is_deleted);
           reunion[`${prod ? 'h' : 'localH'}andlers`].delete(row.id);
           // client.channels.get('500978775878664195').send(`${prod ? '@veryone' : '@veryone'} c'est l'heure de ${row.name}`);
