8ecc01965f7cedc6404d73f113719143b39501c8
CyrilD <cyril.denoyelle@morning.com>
2018-10-20 17:15:10 +0200

    fix msg to row

--- bot.js
+++ bot.js
@@ -19,8 +19,8 @@ const intervalFunc = () => {
   const now = d.hours(new Date(), +2);
   reunion[`${prod ? 'h' : 'localH'}andlers`].list({ noLogs: true }).then(e => {
     e.payload.map(row => {
-      console.log('client.guilds', client.guilds);
-      client.guilds.get(msg.discord_place).channels.find('name', 'reunions').send('coucou');
+      console.log('client.guilds.get(row.discord_place)', client.guilds.get(row.discord_place));
+      client.guilds.get(row.discord_place).channels.find('name', 'reunions').send('coucou');
       if (row && new Date(row.date).getTime() < now && !row.is_deleted) {
         reunion[`${prod ? 'h' : 'localH'}andlers`].delete(row.id);
         client.guild.get(msg.discord_place).send(`${prod ? '@everyone' : '@veryone'} c'est l'heure de ${row.name}`);
