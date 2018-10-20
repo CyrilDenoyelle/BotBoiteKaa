d4be735109010aa5a0996d7844c32af31be9256f
CyrilD <cyril.denoyelle@morning.com>
2018-10-20 16:31:59 +0200

    add createFormater + log for fix

--- db/postgresClient.js
+++ db/postgresClient.js
@@ -10,13 +10,19 @@ const generalFormater = (params) => {
   return arr.join(', ');
 }
 
+const createSqlFormater = (params) => {
+  return `(${Object.keys(params).join('\', \'')}) VALUES (${Object.values(params).join('\', \'')})`;
+
+}
+
 const createReunion = (params) => {
   return new Promise((resolve, rej) => {
     tempClient = client();
     tempClient.connect();
+    console.log('typeof params.date', typeof params.date);
     if (typeof params.date !== 'string') {
-      console.log(`SQL CREATEREUNION => INSERT INTO reunion SET ${generalFormater(params)}`);
-      return tempClient.query(`INSERT INTO reunion SET ${generalFormater(params)}`, (err, res) => {
+      console.log(`SQL CREATEREUNION => INSERT INTO reunion ${createSqlFormater(params)}`); // Object.values(params).join('\', \'')
+      return tempClient.query(`INSERT INTO reunion ${createSqlFormater(params)}`, (err, res) => {
         if (err) rej('createReunion');
         resolve(params);
         tempClient.end();
