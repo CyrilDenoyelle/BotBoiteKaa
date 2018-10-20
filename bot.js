1cda4630c50b63e503976417947644239d605582
CyrilD <cyril.denoyelle@morning.com>
2018-10-20 18:59:48 +0200

    revert emberd shit

--- bot.js
+++ bot.js
@@ -51,20 +51,6 @@ client.on('ready', () => {
 
 client.on('message', msg => {
   msgHandler(msg);
-  if (msg.content.includes('how to embed')) {
-    // We can create embeds using the MessageEmbed constructor
-    // Read more about all that you can do with the constructor
-    // over at https://discord.js.org/#/docs/main/stable/class/RichEmbed
-    const embed = new RichEmbed()
-      // Set the title of the field
-      .setTitle('A slick little embed')
-      // Set the color of the embed
-      .setColor(0xFF0000)
-      // Set the main content of the embed
-      .setDescription('Hello, this is a slick embed!');
-    // Send the embed to the same channel as the message
-    msg.channel.send(embed);
-  }
 });
 
 
