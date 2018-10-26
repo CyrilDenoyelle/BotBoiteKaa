037fffa09ff40a56d6df4678e1f0c1afd8ac53c3
CyrilD <cyril.denoyelle@morning.com>
2018-10-26 23:54:11 +0200

    add template bot response redirectCreate

--- helpers/botResponseTemplates.js
+++ helpers/botResponseTemplates.js
@@ -15,17 +15,23 @@ const deleteReunion = (msg, deleted) => {
   msg.reply(`c'est vous l'doc doc: réunion supprimée \n ${deleted.name} | ${deleted.date}`);
 }
 
+const createRedirect = (msg, created) => {
+  msg.reply(`c'est vous l'doc doc: redirect créée \n ${created.triggering_word} | ${created.from_id} => ${created.to_id}`);
+}
+
 const tutos = {
   createReunion: `pour créer une réunion votre message doit ressembler a ça les biatches: \n"!reunion create pourquoi sans apostrophe, AAAA-MM-JJTHH:mm:ss" ça marche pas si la date est pas dans l'turfu. \n(attention ce truk va faire un "@"everyone sur le discord a l'heure donnée.)`,
   deleteReunion: `pour annuler une réunion: "!reunion delete ID_REUNION." ("!reunion list" affiche toutes les réunions)`,
   listReunion: `"!reunion list" affiche toutes les réunions`,
   reunion: `pour utiliser les fonctions de réunion votre message doit ressembler a ça les biatches: \n"!reunion create pourquoi sans apostrophe, AAAA-MM-JJTHH:mm:ss" \npour annuler une reunion: c'est tres simple aussi "!reunion list" affiche toute les réunions il suffit alors de faire un "!reunion delete ID_REUNION".`,
-  helpReunion: `azerjkqsldfkxcvnorutapiurqkdjwbcxbkhfdgaurieysqgojfsnbmsfh`
+  createRedirect: `bonjour pour créer un redirect veuillez patienter, si vous n'avez pas de réponse sous 3 jours essayez "!redirect create TRIGGERING_WORD, CHANNEL_ID_FROM, CHANNEL_ID_TO`,
+  helpReunion: `azerjkqsldfkxcvnorutapiurqkdjwbcxbkhfdgaurieysqgojfsnbmsfh`,
 }
 
 module.exports = {
   listReunion,
   createReunion,
   deleteReunion,
+  createRedirect,
   tutos
 }
