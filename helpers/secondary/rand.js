5ac3dd7bb68c45c277b0766e84370947e58ac643
CyrilD <cyril.denoyelle@morning.com>
2018-10-27 18:14:48 +0200

    remove logs

--- helpers/secondary/rand.js
+++ helpers/secondary/rand.js
@@ -7,7 +7,6 @@ const on100 = (chances) => {
 const onArray = (arr) => {
   const r = Math.floor(Math.random() * arr.length - 1)
   const out = arr[r];
-  console.log('onArray', r, out);
   return out;
 }
 
