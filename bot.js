9e184abf1b5f269998c8c74d457a20a8a68fca35
CyrilD <cyril.denoyelle@morning.com>
2018-11-04 16:36:42 +0100

    add log for test

--- bot.js
+++ bot.js
@@ -34,6 +34,7 @@ const intervalFunc = () => {
   console.log('now', now);
   reunion[`${prod ? 'h' : 'localH'}andlers`] // if server run in prod call handlers else call localHandlers
     .list({ noLogs: true }).then(e => { // call list function in selected handlsers
+      console.log(e);
       e.payload.map(row => { // iterate on the received list
         if (row && new Date(row.date).getTime() < now && !row.is_deleted) { // if reunion is not deleted and is passed
           reunion[`${prod ? 'h' : 'localH'}andlers`].delete(row.id); // delete reunion
