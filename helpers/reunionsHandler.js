428b1fd86f328256c5fa974bb1fbb05ba78db0a1
CyrilD <cyril.denoyelle@morning.com>
2018-11-04 18:53:00 +0100

    remove comment

--- helpers/reunionsHandler.js
+++ helpers/reunionsHandler.js
@@ -19,7 +19,6 @@ const paramsFormaters = {
     const args = content.slice(16).split(', ');
     if (args.length >= 2) {
       const now = d.hours(new Date(), prod ? 1 : 0);
-      // const now = d.hours(new Date(), 1);
       const date = new Date(args[1]);
       // const reuDate = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()));
 
